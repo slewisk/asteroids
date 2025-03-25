79e2d25cbc545795dfdf021dfa5b77483a5957d9
slewisk <slewisk@gmail.com>
2025-03-25 09:21:44 -0400

    Movement update

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def main():
             
         screen.fill(pygame.Color("Black"))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
